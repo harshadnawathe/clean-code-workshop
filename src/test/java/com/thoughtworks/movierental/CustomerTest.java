9940f04bef62180dad7cae3cf9cd08d6cde73feb
Harshad Nawathe <harshadn@thoughtworks.com>
2019-07-12 09:18:32 +0530

    Adds sample Junit test

--- src/test/java/com/thoughtworks/movierental/CustomerTest.java
+++ src/test/java/com/thoughtworks/movierental/CustomerTest.java
@@ -1,10 +1,15 @@
 package com.thoughtworks.movierental;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class CustomerTest {
     @Test
     public void test() {
+
+        int expected = 3;
+
+        Assert.assertEquals(expected, 1 + 2);
     }
 
 }
