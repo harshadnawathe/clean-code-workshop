f8d08eef0796b605219f4be4d25f4b1dedd36243
safallya <safallya@gmail.com>
2020-01-31 11:22:40 +0530

    Nidhin: Customer junit test

--- src/test/java/com/thoughtworks/movierental/CustomerTest.java
+++ src/test/java/com/thoughtworks/movierental/CustomerTest.java
@@ -6,10 +6,17 @@ import org.junit.Test;
 public class CustomerTest {
     @Test
     public void test() {
-
-        int expected = 3;
-
-        Assert.assertEquals(expected, 1 + 2);
+         Customer customer=new Customer( "safal");
+         customer.addRental(new Rental(new Movie ("Dunkirk", Movie.REGULAR), 7));
+        customer.addRental(new Rental(new Movie ("Dumbo", Movie.CHILDRENS), 7));
+        customer.addRental(new Rental(new Movie ("Jawani", Movie.NEW_RELEASE), 7));
+        String statement = customer.statement();
+        Assert.assertEquals("Rental Record for safal\n" +
+                "\tDunkirk\t9.5\n" +
+                "\tDumbo\t7.5\n" +
+                "\tJawani\t21.0\n" +
+                "Amount owed is 38.0\n" +
+                "You earned 4 frequent renter points",statement);
     }
 
 }
